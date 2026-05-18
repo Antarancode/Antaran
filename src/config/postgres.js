3c4814f4db8ff81340596d8a89907a43674147b0
Antarancode <nicoasell@gmail.com>
2026-05-18 15:11:01 +0200

    Update postgres.js

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -55,7 +55,7 @@ export const pgConfig = {
         
         host: process.env.POSTGRES_HOST || 'localhost',
         port: parseInt(process.env.POSTGRES_PORT) || 5432,
-        database: process.env.POSTGRES_DB || 'titanbot',
+        database: process.env.POSTGRES_DB || '𝔄𝔫𝔱𝔞𝔯𝔞𝔫',
         user: process.env.POSTGRES_USER || 'postgres',
         password: (process.env.POSTGRES_PASSWORD || '').toString(),
         ssl: false,
